b0aaf8f26bd3a630a2d93de22417b6007dd32b24
Kamil Strzempowicz <konserw@gmail.com>
2014-01-28 17:36:18 +0100

    removed old externs

--- common.h
+++ common.h
@@ -4,11 +4,6 @@
 class QString;
 class QStringList;
 
-extern int t;
-extern bool cli;
-extern int fmt;
-extern double rot;
-
 int run(const QString& program, const QStringList& args);
 void save(const QString& fileName, const QString& content);
 
