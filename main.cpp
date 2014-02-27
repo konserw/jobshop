eb47a922c5e8fe69985abe40b56bcea14ef98134
Kamil Strzempowicz <konserw@gmail.com>
2014-02-27 13:56:48 +0100

    quick tests in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,49 @@
 #include <QtDebug>
 #include <QApplication>
 
+//#define TESTRUN 2
+
+#ifdef TESTRUN
+#include "EvolutionWindow.h"
+#include "ResultWindow.h"
+#include "GanttChart.h"
+#include "Jobshop.h"
+#include "JobshopModel.h"
+#include <QFile>
+#include <QDataStream>
+
+void load_sample()
+{
+    QFile file("sample.mar");
+    file.open(QIODevice::ReadOnly);
+    QDataStream in(&file);
+    Jobshop::instance()->model()->loadModel(in);
+}
+
+void fifo_lifo()
+{
+    load_sample();
+    Jobshop::instance()->generateInitialPopulation();
+
+    auto a = Jobshop::instance()->fifoChromosome();
+    ResultWindow win(a);
+    win.save2("/home/konserw/studia/ZZSI/raport/sample/fifo.svg");
+    win.latex2("/home/konserw/studia/ZZSI/raport/sample/fifo.tex");
+
+    auto b = Jobshop::instance()->lifoChromosome();
+    ResultWindow win2(b);
+    win2.save2("/home/konserw/studia/ZZSI/raport/sample/lifo.svg");
+    win2.latex2("/home/konserw/studia/ZZSI/raport/sample/lifo.tex");
+}
+
+int direct_evo()
+{
+    load_sample();
+    EvolutionWindow evo;
+    return evo.exec();
+}
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -16,6 +59,12 @@ int main(int argc, char *argv[])
     else
         qDebug() << "Unable to create Logger instance!";
 
+#if TESTRUN == 1
+    return direct_evo();
+#elif TESTRUN == 2
+    fifo_lifo();
+    return 0;
+#else
     QStringList args = app.arguments();
 
     MainWindow w;
@@ -25,4 +74,5 @@ int main(int argc, char *argv[])
         w.show();
 
     return app.exec();
+#endif
 }
